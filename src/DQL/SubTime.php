7d2542f37b00508d3fc8c76573877a45a0ef74d6
Lua van Loon <lua@luati.cc>
2024-04-13 13:43:07 +0200

    Created SubTime function since DoctrineExtensions doesnt support it (yet)

---
+++ src/DQL/SubTime.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\DQL;
+
+use Doctrine\ORM\Query\AST\Functions\FunctionNode;
+use Doctrine\ORM\Query\Parser;
+use Doctrine\ORM\Query\SqlWalker;
+use Doctrine\ORM\Query\TokenType;
+
+class SubTime extends FunctionNode
+{
+    public $date = null;
+    public $time = null;
+
+    public function parse(Parser $parser): void
+    {
+        $parser->match(TokenType::T_IDENTIFIER);
+        $parser->match(TokenType::T_OPEN_PARENTHESIS);
+        $this->date = $parser->ArithmeticPrimary();
+        $parser->match(TokenType::T_COMMA);
+        $this->time = $parser->ArithmeticPrimary();
+        $parser->match(TokenType::T_CLOSE_PARENTHESIS);
+    }
+
+    public function getSql(SqlWalker $sqlWalker): string
+    {
+        return 'SUBTIME(' .
+            $this->date->dispatch($sqlWalker) . ', ' .
+            $this->time->dispatch($sqlWalker) .
+        ')';
+    }
+}
